21f3d2b6f511d75ee4027e2c50e4022b43fcec7b
Benjamin <benjamin_verhage@yahoo.com>
2019-04-04 11:45:10 +0200

    Determine max value array

--- boucle1.py
+++ boucle1.py
@@ -1,20 +1,21 @@
 """"
 1. Determine even numbers in table
 2. Determine smallest value in table
+3. Determine biggest value in table
 
 """
 # My table
 array = [15, 3, 25, 10, 7, -15, 43, 2, 4, 6]
 
 # Initialise the min as the first item in the array
-minVal = array[0]
+maxVal = array[0]
 
 # iterating each number in list starting at second element
 for num in array[1:]: 
       
     # find minimum in array using While Loop
-    if num < minVal:
-        minVal = num
+    if num > maxVal:
+        maxVal = num
 
 # Print the minValue
-print(minVal)
+print(maxVal)
