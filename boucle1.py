f2f2e73f190be9d9b8f271a1699c08545ad4d3f0
Benjamin <benjamin_verhage@yahoo.com>
2019-04-04 10:16:38 +0200

    Cleansing

--- boucle1.py
+++ boucle1.py
@@ -2,8 +2,8 @@
 Montrer les valeurs pair dans le tableau
 
 """
-
-array = [15, 3, 25, 10, 7, -15]
+# My table
+array = [15, 3, 25, 10, 7, -15, 40, 2, 4, 6]
 
 # iterating each number in list 
 for num in array: 
