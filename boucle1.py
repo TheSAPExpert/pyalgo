e88a78cc330e82f927030c4fcb57068610d0e923
Benjamin <benjamin_verhage@yahoo.com>
2019-04-04 13:59:40 +0200

    Added functions greatest and lowest value of 2 prm

--- boucle1.py
+++ boucle1.py
@@ -2,8 +2,23 @@
 1. Determine even numbers in table
 2. Determine smallest value in table
 3. Determine biggest value in table
+4. Create a 
 
 """
+#Function returns lowest value of two params
+def getLowerOf(firstVal, secondVal):
+    if firstVal < secondVal :
+        return firstVal
+    else:
+        return secondVal
+
+#Function returns the greatest value of two params
+def getGreatestOf(firstVal, secondVal):
+    if firstVal > secondVal :
+        return firstVal
+    else:
+        return secondVal
+
 # My table
 array = [15, 3, 25, 10, 7, -15, 43, 2, 4, 6]
 
