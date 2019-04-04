007801a61c3d97ede5cf6da98b89911c6c11829b
Benjamin <benjamin_verhage@yahoo.com>
2019-04-04 10:15:05 +0200

    Added function to show only EVEN numbers in array

---
+++ boucle1.py
@@ -0,0 +1,13 @@
+""""
+Montrer les valeurs pair dans le tableau
+
+"""
+
+array = [15, 3, 25, 10, 7, -15]
+
+# iterating each number in list 
+for num in array: 
+      
+    # checking condition 
+    if num % 2 == 0: 
+       print(num, end = " ") 
