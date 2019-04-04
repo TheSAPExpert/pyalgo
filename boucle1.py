d3812e37dfb26147609c7f58525f7d9781cc7bd7
Benjamin <benjamin_verhage@yahoo.com>
2019-04-04 14:26:16 +0200

    Function that returns the greater or lower  depends on desc param

--- boucle1.py
+++ boucle1.py
@@ -3,6 +3,7 @@
 2. Determine smallest value in table
 3. Determine biggest value in table
 4. Create greatest and lowest functions
+5. Create a function that extracts greatest and lowest
 
 """
 #Function returns lowest value of two params
@@ -11,13 +12,25 @@ def getLowerOf(firstVal, secondVal):
         return firstVal
     else:
         return secondVal
-
 #Function returns the greatest value of two params
 def getGreatestOf(firstVal, secondVal):
     if firstVal > secondVal :
         return firstVal
     else:
         return secondVal
+
+#Function that returns the greater or lower value of two depensds on desc param
+def compare(firstVal, secondVal, desc=True):
+    if (desc):
+        return getLowerOf(firstVal,secondVal)
+        
+    return getGreaterOf(firstVal, secondVal)
+
+
+#Function that extracts greatest and lowest of array
+def extractorMax&Min(maxVal,minVal):
+
+
 
 # My table
 array = [15, 3, 25, 10, 7, -15, 43, 2, 4, 6]
