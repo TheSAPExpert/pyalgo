1b623c5d00fa6a6fc9a809e359b23f496f0d0c97
Benjamin <benjamin_verhage@yahoo.com>
2019-04-05 09:36:06 +0200

    Init+Variable definition

---
+++ Random password generator.py
@@ -0,0 +1,24 @@
+"""
+---------Password Generator-------
+Create a ps generator that has random outputs.
+Spec:
+    8 caracters min
+    12 max
+    1 Maj mini
+    1 Ponct mini
+    1 number mini
+    must be totally random at each stage
+    the only fct allowed to use is random
+------------------------------------
+
+"""
+#VARIABLES#
+alphabetLowerC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alphabetUpperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+number = [0,1,2,3,4,5,6,7,8,9]
+puncList = [".",";",":","!","?","/","\\",",","#","@","$","&",")","(","\""]
+tablesize =
+
+
+
+#EXECUTION#
