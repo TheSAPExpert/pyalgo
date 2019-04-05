9aeb08a7d5958afa152eac4f7db1ee5b62055b5e
Benjamin <benjamin_verhage@yahoo.com>
2019-04-05 15:03:36 +0200

    v2 final

--- Random password generator.py
+++ Random password generator.py
@@ -12,16 +12,45 @@ Spec:
 ------------------------------------
 
 """
+#IMPORTS#
+import random
+
 #VARIABLES#
 alphabetLowerC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 alphabetUpperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 number = [0,1,2,3,4,5,6,7,8,9]
 puncList = [".",";",":","!","?","/","\\",",","#","@","$","&",")","(","\""]
-psLenght = random.randint(8,12)
-
 
 #EXECUTION#
-import random
+#Define lenght password
+psLenght = random.randint(8,12)    
+print("Length password:", psLenght)
 
-passwordEmpty = list(range(psLenght))
-print(passwordEmpty, end='')
+#Define random items of password
+Lower = alphabetLowerC[random.randint(0,25)]
+
+Upper = alphabetUpperC[random.randint(0,25)]
+
+Nmbr = number[random.randint(0,8)]
+
+Punct = puncList[random.randint(0,14)]
+
+#Define random tables, confirm that each table is used, create password
+password = [Lower, Upper, Nmbr, Punct]
+
+while len(password) < psLenght:
+    master = random.randint(0,3)
+    if master == 0:
+        slave = alphabetLowerC
+    elif master == 1:
+        slave = alphabetUpperC
+    elif master == 2:
+        slave  = number
+    elif master == 3:
+        slave = puncList
+    randomNum = random.randint(0, len(slave)-1)
+    password.append(slave[randomNum])
+
+#Shuffle tables
+random.shuffle(password)
+print(*password, sep="")
